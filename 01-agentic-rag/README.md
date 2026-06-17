d54c0ad4cf09cb386d4a41533ba7ed98c98ae5d5
Shanice <30316866+swilldd@users.noreply.github.com>
2026-06-17 10:12:02 +0100

    completed module 1 homework

--- 01-agentic-rag/README.md
+++ 01-agentic-rag/README.md
@@ -2,3 +2,5 @@
 
 In this module we explore how we can use the strengths and limitations of LLMs and how we can improve the usefulness, by implementing RAG pipelines as well as extending this further through the addition of agentic loops.
 
+Part 1: RAG
+- 
---
+++ 01-agentic-rag/ingest_hwk.py
@@ -0,0 +1,35 @@
+from minsearch import Index
+from gitsource import GithubRepositoryDataReader
+
+
+def load_course_data():
+    """ Load data from Data talks club LLM Zoomcamp.
+    Returns a list of dicts with md file content and filename.
+    """
+    
+    reader = GithubRepositoryDataReader(
+        repo_owner="DataTalksClub",
+        repo_name="llm-zoomcamp",
+        commit_id="8c1834d",
+        allowed_extensions={"md"},
+        filename_filter=lambda path: "/lessons/" in path,
+    )
+
+    files = reader.read()
+
+    documents = []
+
+    for file in files:
+        doc = file.parse()
+        documents.append(doc)
+
+    return documents
+
+def build_index(documents):
+    index = Index(
+        text_fields=['content'],
+        keyword_fields=['filename']
+    )
+
+    index.fit(documents)
+    return index
